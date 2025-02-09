e7c622fbd8febbea7d66b60d2d5935f17c52dbd9
tiyaadornments <tiyaadornments@gmail.com>
2025-02-09 17:38:23 +0000

    firebas eadd

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/cart.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:myapp/firebase_options.dart' show DefaultFirebaseOptions;
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(EarringsShopApp());
 }
 
